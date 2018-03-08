a44a2346027c8feb402406bf698ca391ff1224e6
sebraddo <37003538+sebraddo@users.noreply.github.com>
2018-03-08 12:31:42 -0800

    Update DatePicker.Basic.Example.tsx

--- packages/office-ui-fabric-react/src/components/DatePicker/examples/DatePicker.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/DatePicker/examples/DatePicker.Basic.Example.tsx
@@ -79,7 +79,7 @@ export class DatePickerBasicExample extends React.Component<{}, IDatePickerBasic
 
     return (
       <div>
-        <DatePicker displayDatePickerFormat={ DatePickerFormat.dateOnly } setSelectedDateTime={ this._timeChangeCallback } firstDayOfWeek={ firstDayOfWeek } onAfterMenuDismiss={ this.consolelog1 } allowTextInput strings={ DayPickerStrings } showGoToToday isMonthPickerVisible placeholder='Select a date...' />
+        <DatePicker displayDatePickerFormat={ DatePickerFormat.dateOnly } setSelectedDateTime={ this._timeChangeCallback } firstDayOfWeek={ firstDayOfWeek } onAfterMenuDismiss={ this.onAfterMenuDismissLog } allowTextInput strings={ DayPickerStrings } showGoToToday isMonthPickerVisible placeholder='Select a date...' />
         <Dropdown
           label='Select the first day of the week'
           options={ [
@@ -123,8 +123,8 @@ export class DatePickerBasicExample extends React.Component<{}, IDatePickerBasic
     console.log(date);
   }
 
-  private consolelog1() {
-    console.log("onAfterMenudismiss called");
+  private onAfterMenuDismissLog() {
+    console.log("onAfterMenuDismiss called");
     return;
   }
 
