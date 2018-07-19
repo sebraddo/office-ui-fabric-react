f8526d923821f676d6dc7cae2865ee50ade72ec1
Seleste Braddock <sebraddo@microsoft.com>
2018-07-19 13:37:00 -0700

    merge

--- packages/office-ui-fabric-react/src/components/ComboBox/ComboBox.tsx
+++ packages/office-ui-fabric-react/src/components/ComboBox/ComboBox.tsx
@@ -757,12 +757,7 @@ export class ComboBox extends BaseComponent<IComboBoxProps, IComboBoxState> {
   @autobind
   private _select() {
     this._comboBox.inputElement.select();
-<<<<<<< HEAD
-    this.focus(true);
-
-=======
     let newState = undefined;
->>>>>>> c6dde3a036deddaef170076354835cc59dd67cfd
     if (!this.state.focused) {
       newState = Object.assign({}, { focused: true });
     }
